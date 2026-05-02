a6475d36a5d9fdecfc9c4b5e370ba3ce608b02f3
resenhandodirei <larimscorrea@gmail.com>
2026-05-02 13:05:00 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header'; // Verifique o caminho
+import { Hero } from './components/hero/hero'; // Verifique o caminho
+import { Projects } from './components/projects/projects';
+import { Services } from './components/services/services';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Header, Hero, Projects, Services, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
+
 export class App {
-  protected readonly title = signal('meu-portfolio');
+  protected readonly title = signal('Design && Portfolio');
 }
---
+++ src/app/components/hero/hero.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router'; // Importação necessária
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [RouterOutlet], // Tem que avisar o Angular aqui
+  templateUrl: './hero.html',
+  styleUrl: './hero.scss'
+})
+export class Hero {}
